19eb89f15767e5f5c89878c7b46997c08cfc4f51
jperez1478 <jessicperez3@gmail.com>
2022-06-08 00:01:09 -0500

    Initial Commit

---
+++ Cat or Dog?/Cat_or_Dog_App.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_or_Dog_App.swift
+//  Cat or Dog?
+//
+//  Created by Jessica Perez on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Cat_or_Dog_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat or Dog?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cat or Dog?
+//
+//  Created by Jessica Perez on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
